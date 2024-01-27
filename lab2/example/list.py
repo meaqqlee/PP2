e8a4d5e63e127eaaec26c50d2da4a1bf4694d892
meaqqlee <156634474+meaqqlee@users.noreply.github.com>
2024-01-27 08:01:11 +0600

    Add files via upload

---
+++ lab2/example/list.py
@@ -0,0 +1,32 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+#print the second item from list
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+#change the first element to "kiwi"
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+#operation append
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+#operation insert to add into exact position
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+#operaion remove
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+#use negative index to print
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+#range indexing
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+#use len() to find the length
+
---
+++ lab2/exercise/boolean.py
@@ -0,0 +1,15 @@
+print(10>9)
+#it will print the boolean value as a "TRUE"
+
+print(10==9)
+#False
+
+print(10<9)
+#False
+
+print(bool("abc"))
+#since the string "abc" is non-empty, the expression will be True
+
+print(bool(0))
+#since int value converted into bool value is non-zero integer it will print True, so its False
+
