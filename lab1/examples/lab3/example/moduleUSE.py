2e965fc8b63155a92a777d1004b243123005a949
meaqqlee <magzhanmmm@gmail.com>
2024-04-08 03:36:33 +0600

    update 9

--- lab1/examples/lab3/example/moduleUSE.py
+++
@@ -1,9 +0,0 @@
-import mymodule as mx
-
-mymodule.greeting("Magzhan")
-
-x = mymodule.person1["name"]
-print(x)
-
-x = mx.person1["name"]
-print(x)
--- lab3/example/mymodule.py
+++
@@ -1,8 +0,0 @@
-def greeting(name):
-    print("hello, " + name)
-    
-person1 = {
-    "name" : "Magzhan",
-    "age" : "18",
-    "country" : "Kazakhstan"
-}
